1b4684ac9e6c59c5463058d23fb27db5b91843c4
andhiniwputri <103379696+andhiniwputri@users.noreply.github.com>
2022-06-18 23:53:20 +0700

    Add files via upload

---
+++ src/tugaspbo9/Magician.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaspbo9;
+
+/**
+ *
+ * @author Acer
+ */
+public class Magician extends Character {
+     Magician (){
+        setHP(100);
+        setAttack(60);
+        setDefense(10);
+    }
+
+    @Override
+    public boolean attack() {
+        double counter = Math.random() * 10;
+        if (counter > 0 && counter <= 3.5)
+            return true;
+        else
+            return false;
+    }
+}
+
+
---
+++ src/tugaspbo9/Warrior.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaspbo9;
+
+/**
+ *
+ * @author Acer
+ */
+public class Warrior extends Character {
+    Titan(){
+        setHP(200);
+        setAttack(45);
+        setDefense(0);
+    }
+
+    @Override
+    public boolean attack() {
+        double counter = Math.random() * 10;
+        if (counter > 0 && counter <= 4.0)
+            return true;
+        else
+            return false;
+    }
+}
+
+
